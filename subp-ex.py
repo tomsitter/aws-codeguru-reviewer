fb2149d9f566fdd4df9f0c866ae68c26336b0504
Tom Sitter <thomas.sitter@gmail.com>
2022-04-22 14:39:41 -0700

    Fix security and memory issues

--- subp-ex.py
+++ subp-ex.py
@@ -1,29 +1,41 @@
+import shlex
 import subprocess
 import sys
 import boto3
-import datetime
+from datetime import datetime, timezone
 
 s3 = boto3.client('s3')
 
 
 def main(argv):
-    cmd = argv
-    log_file_name = datetime.datetime.now().strftime("%m_%d_%Y") + "_logfile"
+    cmd = determine_command(int(sys.argv[1]))
+    log_file_name = datetime.now(tz=timezone.utc).strftime("%m_%d_%Y") + "_logfile"
     kickoff_subprocess(cmd, log_file_name)
     upload_output_to_S3(log_file_name)
 
 
+def determine_command(user_input):
+    if user_input == 1:
+        return ['echo', "command1"]
+    if user_input == 2:
+        return ['echo', "command2"]
+    if user_input == 3:
+        return ['echo', "command3"]
+    else:
+        return ['echo', "default"]
+
+
 def kickoff_subprocess(cmd, log_file_name):
-    process = subprocess.call(cmd, shell=True)
-    file = open(log_file_name, "a+")
-    timestamp = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    process = subprocess.call(cmd, shell=False)
+    timestamp = datetime.now(tz=timezone.utc).strftime("%m/%d/%Y, %H:%M:%S")
     output = timestamp + " Command: " + cmd[0] + " | Return Code: " + str(process) + "\n"
-    file.write(output)
+    with open(log_file_name, "a+") as f:
+        f.write(output)
 
 
 def upload_output_to_S3(log_file_name):
-    f = open(log_file_name, "rb")
-    s3.upload_fileobj(f, "<FMI1>", log_file_name)
+    with open(log_file_name, "rb") as f:
+        s3.upload_fileobj(f, "<FMI1>", log_file_name)
 
 
 if __name__ == "__main__":
